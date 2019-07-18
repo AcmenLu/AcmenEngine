1df9c07e961a78f7a5b09cec3fe8faeb90800583
AcmenLu <1109325089@qq.com>
2019-07-18 09:24:49 +0800

    Update HelloEngine_d3d12.cpp

--- Platform/Windows/HelloEngine_d3d12.cpp
+++ Platform/Windows/HelloEngine_d3d12.cpp
@@ -11,9 +11,7 @@
 #include <DirectXMath.h>
 #include <DirectXPackedVector.h>
 #include <DirectXColors.h>
-
 #include <wrl/client.h>
-
 #include <string>
 #include <exception>
 
@@ -47,6 +45,9 @@ namespace Acmen
 using namespace DirectX;
 using namespace DirectX::PackedVector;
 using namespace Acmen;
+using namespace Microsoft::WRL;
+using namespace std;
+
 
 // const value declaration.
 const uint32_t nScreenWidth = 960;
@@ -55,8 +56,108 @@ const uint32_t nFrameCount = 2;
 const bool bUseWarpDevice = true;
 
 // global value declaration
-D3D12_VIEWPORT				g_ViewPort = { 0.0f, 0.0f, static_cast<float>(nScreenWidth), static_cast<float>(nScreenHeight)};
-D3D12_RECT					g_ScissorRect = {0, 0, nScreenWidth, nScreenHeight};
+D3D12_VIEWPORT					g_ViewPort = { 0.0f, 0.0f, static_cast<float>(nScreenWidth), static_cast<float>(nScreenHeight)};
+D3D12_RECT						g_ScissorRect = {0, 0, nScreenWidth, nScreenHeight};
+ComPtr<IDXGISwapChain3>			g_pSwapChain = nullptr;
+ComPtr<ID3D12Device>			g_pDevice = nullptr;
+ComPtr<ID3D12Resource>			g_pRenderTargets[nFrameCount];
+ComPtr<ID3D12CommandAllocator>	g_pCommandAllocator;
+ComPtr<ID3D12CommandQueue>		g_pCommandQueue;
+ComPtr<ID3D12RootSignature>		g_pRootSignature;
+ComPtr<ID3D12DescriptorHeap>	g_pRtvHeap;
+ComPtr<ID3D12PipelineState>		g_pPipelineState;
+
+ComPtr<ID3D12GraphicsCommandList1>	g_pCommandList;
+
+uint32_t						g_nRtvDescriptorSize;
+
+ComPtr<ID3D12Resource>			g_pVertexBuffer;
+D3D12_VERTEX_BUFFER_VIEW		g_VertexBufferView;
+uint32_t						g_nFrameIndex;
+HANDLE							g_hFenceEvent;
+ComPtr<ID3D12Fence>				g_pFence;
+uint32_t						g_nFenceValue;
+struct VERTEX {
+	XMFLOAT3	Position;
+	XMFLOAT4	Color;
+};
+
+wstring 						g_AssetsPath;
+std::wstring GetAssetsfullPath(LPCWSTR assetName)
+{
+	return g_AssetsPath + assetName;
+}
+
+void GetAssetPath(WCHAR* path, UINT pathSize)
+{
+	if(path == nullptr)
+	{
+		throw std::exception();
+	}
+	DWORD size = GetModuleNameW(nullptr, path, pathSize);
+	if(size == 0 || size == pathSize)
+	{
+		throw std::exception();
+	}
+	WCHAR* lastSlash = wcsrchr(path, L"\\");
+	if(lastSlash)
+	{
+		*(lastSlash + 1) = L"\0";
+	}
+}
+
+void WaitForPreviousFrame()
+{
+	const uint64_t fence = g_nFenceValue;
+	ThrowIfFailed(g_pCommandQueue->Signal(g_pFence.Get(), fence));
+	g_nFenceValue++;
+	if(g_pFence->GetCompletedValue() < fence)
+	{
+		ThrowIfFailed(g_pFence->SetEventOnCompletion(fence, g_hFenceEvent));
+		WaitForSingleObject(g_hFenceEvent, INFINITE);
+	}
+	g_nFrameIndex = g_pSwapChain->GetCurrentBackBufferIndex();
+}
+
+void CreateRenderTarget()
+{
+	D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
+	rtvHeapDesc.NumDescriptors = nFrameCount;
+	rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+	rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+	ThrowIfFailed(g_pDevice->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&g_pRtvHeap)));
+	g_nRtvDescriptorSize = g_pDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(g_pRtvHeap->GetCPUDescriptorHandleForHeapStart());
+	for(uint32_t i = 0; i <nFrameCount; i ++)
+	{
+		ThrowIfFailed(g_pSwapChain->GetBuffer(i, IID_PPV_ARGS(&g_pRenderTargets[i])))
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
