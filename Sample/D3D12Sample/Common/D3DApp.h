2ad6c1dbb39905290f16247f4925c3122590a98b
AcmenLu <1109325089@qq.com>
2019-07-23 13:42:26 +0800

    D3D12

--- Sample/D3D12Sample/Common/D3DApp.h
+++ Sample/D3D12Sample/Common/D3DApp.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "d3dUtil.h"
 #include "GameTimer.h"
 
@@ -10,16 +12,52 @@ class D3DApp
 {
 protected:
     D3DApp(HINSTANCE hInstance);
+	D3DApp(const D3DApp& rhs) = delete;
+	virtual ~D3DApp();
     
 public:
-    D3DApp(/* args */);
-    ~D3DApp();
+	static D3DApp* GetApp();
+	HINSTANCE	AppInst() const;
+	HWND		MainWnd() const;
+	float		AspectRation() const;
+	bool		Get4xMsaaState() const;
+	void		Set4xMsaaState(bool value);
+	int			Run();
+	virtual bool Initialize();
+	virtual LRESULT MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+protected:
+	virtual void CreateRtvAndDsvDescriptorHeaps();
+	virtual void OnResize();
+	virtual void Update(const GameTimer& gt) = 0;
+	virtual void Draw(const GameTimer& gt) = 0;
+
+	virtual void OnMouseDown(WPARAM btnState, int x, int y) {}
+	virtual void OnMouseUp(WPARAM btnState, int x, int y) {}
+	virtual void OnMouseMove(WPARAM btnState, int x, int y) {}
+
+protected:
+	bool InitMainWindow();
+	bool InitDirect3D();
+	void CreateCommandObjects();
+	void CreateSwapChain();
+
+	void FlushCommandQueue();
+
+	ID3D12Resource* CurrentBackBuffer() const;
+	D3D12_CPU_DESCRIPTOR_HANDLE CurrentBackBufferView() const;
+	D3D12_CPU_DESCRIPTOR_HANDLE DepthStencilView() const;
+
+	void CalculateFrameStates();
+
+	void LogAdapters();
+	void LogAdapterOutPuts(IDXGIAdapter* adapter);
+	void LogDisplayModes(IDXGIOutput* output, DXGI_FORMAT format);
+
+protected:
+	static D3DApp* mApp;
+
+	HINSTANCE	mhAppInst = nullptr;
+	HWND		mhMainWnd = nullptr;
+
 };
-
-D3DApp::D3DApp(/* args */)
-{
-}
-
-D3DApp::~D3DApp()
-{
-}
---
+++ Sample/D3D12Sample/Common/GameTimer.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class GameTimer
+{
+public:
+	GameTimer();
+	~GameTimer();
+
+private:
+
+};
